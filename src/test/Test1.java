dff6cdf24e5a2520c5d1999dc8766a4a2e5a555b
KOSMO <skan0421@gmail.com>
2023-04-06 16:21:58 +0900

    If문으로 성적 구하기

---
+++ src/test/Test1.java
@@ -0,0 +1,43 @@
+package test;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Test1 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		try {
+		    System.out.print("점수를 입력하세요 (0~100점): ");
+		    int score = sc.nextInt();
+		    
+		    if (score < 0 || score > 100) {
+		        System.out.println(score + " 잘못된 점수입니다.");
+		    } else {
+		        char grade;
+		        switch (score / 10) {
+		            case 10:
+		            case 9:
+		                grade = 'A';
+		                break;
+		            case 8:
+		                grade = 'B';
+		                break;
+		            case 7:
+		                grade = 'C';
+		                break;
+		            case 6:
+		                grade = 'D';
+		                break;
+		            default:
+		                grade = 'F';
+		        }
+		        System.out.println("점수: " + score + " 학점: " + grade);
+		    }
+		} catch (InputMismatchException e) {
+		    System.out.println("정수 값을 입력해주세요.");
+		} finally {
+		    sc.close();
+		}
+	}
+}
